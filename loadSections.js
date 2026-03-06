2092dd130eb3d62cc599e634fb636a361450a70d
Shova24 <shovaaziz2409@gmail.com>
2026-03-07 04:07:12 +0600

    fix: update style folder

--- loadSections.js
+++ loadSections.js
@@ -12,13 +12,13 @@ function loadSection(id, file) {
     .catch(error => console.error(error));
 }
 
-loadSection("hero", "/sections/hero.html");
-loadSection("highlights", "/sections/highlights.html");
-loadSection("motorcycles", "/sections/motorcycles.html");
-loadSection("motoculture", "/sections/motoculture.html");
-loadSection("rides", "/sections/rides.html");
-loadSection("latest", "/sections/latest.html");
-loadSection("legacy", "/sections/legacy.html");
-loadSection("locate", "/sections/locate.html");
-loadSection("shop", "/sections/shop.html");
-loadSection("footer", "/sections/footer.html");
+loadSection("hero", "./sections/hero.html");
+loadSection("highlights", "./sections/highlights.html");
+loadSection("motorcycles", "./sections/motorcycles.html");
+loadSection("motoculture", "./sections/motoculture.html");
+loadSection("rides", "./sections/rides.html");
+loadSection("latest", "./sections/latest.html");
+loadSection("legacy", "./sections/legacy.html");
+loadSection("locate", "./sections/locate.html");
+loadSection("shop", "./sections/shop.html");
+loadSection("footer", "./sections/footer.html");
